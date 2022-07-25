dd90057ee3daf390368d929e5c6ee9ef7e1adde3
shengiv <66622728+shengiv@users.noreply.github.com>
2022-07-25 17:08:34 +0800

    CORS error fix
    reverse proxy server created

---
+++ reverse-proxy/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors({
+    origin: '*'
+}))
+
+app.get(':endpoint([\\/\\w\\.-]*)', function(req, res){
+    let endpoint = process.env.API_BASE_URL + req.params.endpoint
+    let params = {}
+    
+    if(!!process.env.API_KEY_PARAM_NAME && !!process.env.API_KEY){
+        params[process.env.API_KEY_PARAM_NAME] = process.env.API_KEY
+    }
+
+    for(const [field, value] of Object.entries(req.query)){
+        params[field] = value
+    }
+
+    axios.get(endpoint, {
+        params: params
+    }).then(response => {
+        res.json(response.data)
+    }).catch(error =>{
+        res.json(error)
+    })
+})
+
+app.listen(3000)
